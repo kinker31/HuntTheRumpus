914b9f7ec0f80d9dc4655ebe52d4b1e2f4fdb5a8
kinker31 <dp223171@gmail.com>
2024-07-25 04:56:14 -0500

    Basic curses implementation, looks promising so far!

--- mainlib.h
+++ mainlib.h
@@ -3,3 +3,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <unistd.h>
+#include <curses.h>
+
+void PrintCommands()
+{
+    printw("[H]elp: Prints this screen.\n");
+    printw("[O]ptions: Opens up the options, which has a menu of it's own.\n");
+    printw("[N]ew Game: \n");
+    printw("[C]ustom Maze: \n");
+    printw("Please use lowercase keys when selecting, and this menu might change in the future!\n");
+    printw("Press any key to continue.\n");
+    char dummyValue = getch();
+}
--- mainprog.c
+++ mainprog.c
@@ -2,7 +2,30 @@
 
 int main()
 {
-    system("clear");
-    printf("Welcome to Hunt the Rumpus!\n");
+    initscr(); noecho();
+    printw("Welcome to Hunt the Rumpus!\n"); refresh();
+    sleep(1);
+    char selection;
+    unsigned short int options;
+    while(selection != 'q')
+    {
+        clear();
+        printw("Make a selection, or press h for help: ");
+        refresh();
+        selection = getch();
+        printw("%c\n", selection);
+        switch(selection)
+        {
+            case 'h':
+                PrintCommands();
+                break;
+            case 'q':
+                printw("Well, it's been fun, see you next time!\n"); refresh();
+                sleep(1);
+            default:
+                break;
+        }
+    }
+    endwin();
     return 0;
 }
